cb80793a0070e8057d8202617fcf0a9dc9f51ae0
lopee50 <tobias.wirtz1@gmail.com>
2014-11-19 00:59:39 +0100

    Added support for <h2> and appending to file.

--- NYTimesStoryScraper.py
+++ NYTimesStoryScraper.py
@@ -3,14 +3,21 @@ import requests
 
 page = requests.get('http://international.nytimes.com')
 htmlFile = html.fromstring(page.text)
-file = open("NYTimesStories.txt", "w")
+
 
 stories = htmlFile.xpath('//h3[@class="story-heading"]/text()')
+stories2 = htmlFile.xpath('//h2[@class="story-heading"]/text()')
 
 for i in range(0, len(stories)):
     stories_string = str(stories[i])
+    stories2_string = str(stories2[i])
     if "\n" in stories_string:
         stories_string.replace("\n", "")
-        file.write(stories_string)
+        with open("NYTimesStories.txt", "a") as f:
+            f.write(stories_string)
+    if "\n" in stories2_string:
+        stories2_string.replace("\n", "")
+        with open("NYTimesStories.txt", "a") as f:
+            f.write(stories2_string)
 
 file.close()
